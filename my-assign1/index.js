ce272a6c88736dbb19c0e3184907347903a0352e
harleenkaur1215 <157448095+harleenkaur1215@users.noreply.github.com>
2025-08-17 18:53:09 +0530

    my work

---
+++ my-assign1/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+const connectDB = require("./db/connectDb");
+require("dotenv").config();
+const path = require("path");
+//routers
+// const userRouter = require("./routes/user.routes");
+const todoRouter = require("./routes/todo.route");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,"public")))
+
+// app.use("/user",userRouter);
+app.use("/todo",todoRouter);
+
+connectDB().then(()=>{
+  app.listen(PORT, () => console.log("Server running on port " + PORT));
+})
+.catch((err)=>console.log(err))
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
+
